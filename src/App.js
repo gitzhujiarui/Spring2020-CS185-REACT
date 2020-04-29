22ce739cc0438b67f7f422fe1f26da17163e1c26
gitzhujiarui <zhu.jiarui@outlook.com>
2020-04-28 20:03:00 -0700

    jz - minor changes to App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React, { Component } from 'react';
 import './App.css'
 import TabList from './component/TabList';
 import Body from './component/Body';
-// import black_hole from './images/black_hole.jpg'
-// import darkenergy from './images/darkenergy.jpg'
-// import galaxy from './images/galaxy.jpg'
-// import gravitational_wave from './images/gravitational_wave.jpeg'
-// import massive_black_holes from './images/massive_black_holes.jpg'
-// import universe_2 from './images/universe_2.jpeg'
-// import universe_3 from './images/universe_3.jpg'
-// import galaxy_2 from './images/galaxy_2.jpeg'
-// import universe_4 from './images/universe_4.jpg'
 
 export class App extends Component {
   constructor(){
